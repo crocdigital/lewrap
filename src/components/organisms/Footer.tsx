6e217fbbc02f1b70b7453eeb82b980a72be5867b
crocdigital <hello@crocdigital.com.au>
2025-12-08 23:47:00 +1100

    Committed to staging branch

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
                 </div>
 
                 <div className="col-span-2">
-                    <h3 className="text-5xl opacity-30 -mt-1">Fresh, high quality, delicious meals without the wait.</h3>
+                    <h3 className="text-5xl opacity-30 -mt-1">Fresh, high quality, delicious, meals without the wait.</h3>
                 </div>
 
                 <div className="col-span-1 hidden md:block">
