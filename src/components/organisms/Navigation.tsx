d2aeca3d1b0f08f09c98cc74d4e6cab1f3f03c70
crocdigital <hello@crocdigital.com.au>
2025-12-10 11:04:40 +1100

    Updated social links

--- src/components/organisms/Navigation.tsx
+++ src/components/organisms/Navigation.tsx
@@ -128,16 +128,16 @@ export default function Navigation() {
 
                     {/* Social Icons - Desktop: next to logo, Mobile: centered with equal spacing */}
                     <div className="hidden md:flex items-center gap-3 shrink-0 ml-5">
-                        <Link href="#" className="hover:opacity-70 transition-opacity"><Instagram className="w-5 h-5" /></Link>
-                        <Link href="#" className="hover:opacity-70 transition-opacity"><TikTok className="w-5 h-5" /></Link>
-                        <Link href="#" className="hover:opacity-70 transition-opacity"><Facebook className="w-5 h-5" /></Link>
+                        <Link href="https://www.instagram.com/lewrap_official" target="_blank" className="hover:opacity-70 transition-opacity"><Instagram className="w-5 h-5" /></Link>
+                        <Link href="https://www.tiktok.com/@lewrap" target="_blank" className="hover:opacity-70 transition-opacity"><TikTok className="w-5 h-5" /></Link>
+                        <Link href="https://www.facebook.com/lewrapaus" target="_blank" className="hover:opacity-70 transition-opacity"><Facebook className="w-5 h-5" /></Link>
                     </div>
 
                     {/* Social Icons - Mobile: centered absolutely */}
                     <div className="md:hidden absolute left-1/2 -translate-x-1/2 flex items-center gap-5">
-                        <Link href="#" className="hover:opacity-70 transition-opacity"><Instagram className="w-5 h-5" /></Link>
-                        <Link href="#" className="hover:opacity-70 transition-opacity"><TikTok className="w-5 h-5" /></Link>
-                        <Link href="#" className="hover:opacity-70 transition-opacity"><Facebook className="w-5 h-5" /></Link>
+                        <Link href="https://www.instagram.com/lewrap_official" className="hover:opacity-70 transition-opacity"><Instagram className="w-5 h-5" /></Link>
+                        <Link href="https://www.tiktok.com/@lewrap" className="hover:opacity-70 transition-opacity"><TikTok className="w-5 h-5" /></Link>
+                        <Link href="https://www.facebook.com/lewrapaus" className="hover:opacity-70 transition-opacity"><Facebook className="w-5 h-5" /></Link>
                     </div>
 
                     {/* Spacer - Desktop only */}
